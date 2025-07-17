4e6189205b80205f766b60c3c10fd182c9ba51c3
nachocjs <iramos99106@gmail.com>
2025-07-16 22:13:26 -0300

    final

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -4,7 +4,7 @@ import Cart from "../models/cart.model.js";
 
 const router = Router();
 
-// Vista principal con filtros, ordenamiento y paginación
+
 router.get("/", async (req, res) => {
   try {
     const { limit = 4, page = 1, sort, query, cid } = req.query;
@@ -34,7 +34,7 @@ router.get("/", async (req, res) => {
     const result = await Product.paginate(filter, options);
 
     res.render("home", {
-      cartId: cid || "", // Esto lo necesitás para agregar productos al carrito
+      cartId: cid || "",
       products: result.docs,
       totalPages: result.totalPages,
       page: result.page,
@@ -60,25 +60,6 @@ router.get("/realTimeProducts", async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).send("Error cargando productos en tiempo real");
-  }
-});
-
-// Vista de detalle de un producto con botón para agregar al carrito
-router.get("/products/:pid", async (req, res) => {
-  try {
-    const pid = req.params.pid;
-    const product = await Product.findById(pid).lean();
-
-    if (!product) {
-      return res.status(404).send("Producto no encontrado");
-    }
-
-    const cartId = req.query.cid || ""; // Cart ID opcional como query
-
-    res.render("productDetail", { product, cartId });
-  } catch (error) {
-    console.error("Error al cargar el producto:", error);
-    res.status(500).send("Error al cargar el producto");
   }
 });
 
